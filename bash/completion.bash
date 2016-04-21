1fbce632e4899da45f2936efa497fac58aea2f88
Simon Downes <simon.downes@gamer-network.net>
2016-04-21 10:56:32 +0100

    Added support for autocompletion of git aliases in bash

--- bash/completion.bash
+++ bash/completion.bash
@@ -3,3 +3,16 @@
 [ -f /etc/bash_completion ] && source /etc/bash_completion
 
 # TODO: any other tab completion stuff goes here...
+
+# Autocompletion for git aliases
+# http://stackoverflow.com/questions/342969/how-do-i-get-bash-completion-to-work-with-aliases
+if [ -f ~/.git-completion.bash ]; then
+
+	. ~/.git-completion.bash
+
+	__git_complete gl _git_log
+	__git_complete ga _git_add
+	__git_complete gd _git_diff
+	__git_complete gdc _git_diff
+
+fi
